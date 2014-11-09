343e5c9d919a1ec44586cde0c65c8b4793675c60
AlanTai <rainman.tai@gmail.com>
2014-11-09 12:49:17 -0800

    update bluetooth and gcm app

--- prjCandyHouse/gen/com/prjcandyhouse/R.java
+++ prjCandyHouse/gen/com/prjcandyhouse/R.java
@@ -1091,11 +1091,15 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f050041;
+        public static final int action_settings=0x7f050047;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
+        public static final int btnPaired=0x7f050044;
+        public static final int btnSearchCancel=0x7f050045;
         public static final int btnSubmitRegId=0x7f050040;
+        public static final int btnTurnOff=0x7f050043;
+        public static final int btnTurnOn=0x7f050042;
         public static final int checkbox=0x7f05002d;
         public static final int collapseActionView=0x7f05000d;
         public static final int default_activity_button=0x7f050028;
@@ -1112,6 +1116,7 @@ containing a value of this type.
         public static final int ifRoom=0x7f05000a;
         public static final int image=0x7f050027;
         public static final int listMode=0x7f050001;
+        public static final int listView=0x7f050046;
         public static final int list_item=0x7f050029;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
@@ -1143,6 +1148,7 @@ containing a value of this type.
         public static final int tvMessageLabel=0x7f05003e;
         public static final int tvRegStatus=0x7f05003c;
         public static final int tvRegStatusResult=0x7f05003d;
+        public static final int txtBluetoothStatus=0x7f050041;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
         public static final int withText=0x7f05000c;
@@ -1249,9 +1255,14 @@ containing a value of this type.
         public static final int broadcastLabel=0x7f0a0012;
         public static final int broadcastResult=0x7f0a0013;
         public static final int btnSubmitRegId=0x7f0a0014;
+        public static final int btnTurnOff=0x7f0a0017;
+        public static final int btnTurnOn=0x7f0a0016;
+        public static final int findDevices=0x7f0a0019;
         public static final int hello_world=0x7f0a000e;
+        public static final int pairedList=0x7f0a0018;
         public static final int registryStatus=0x7f0a0010;
         public static final int registryStatusResult=0x7f0a0011;
+        public static final int txtBluetoothStatus=0x7f0a0015;
     }
     public static final class style {
         /** 
