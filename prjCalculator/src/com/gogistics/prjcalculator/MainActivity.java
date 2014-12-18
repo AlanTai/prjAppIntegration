d3cb86667c942102769b9d30430056d5df6510cf
AlanTai <rainman.tai@gmail.com>
2014-12-18 12:10:02 -0800

    update Automation Test Example

--- prjCalculator/src/com/gogistics/prjcalculator/MainActivity.java
+++ prjCalculator/src/com/gogistics/prjcalculator/MainActivity.java
@@ -1,16 +1,29 @@
 package com.gogistics.prjcalculator;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.MotionEvent;
+import android.view.View;
+import android.view.View.OnTouchListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
+	
+	/* inner variables */
+	float vNum1, vNum2, cOutPut;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//
+		initXMLComponents();
 	}
 
 	@Override
@@ -30,5 +43,44 @@ public class MainActivity extends Activity {
 			return true;
 		}
 		return super.onOptionsItemSelected(item);
+	}
+	
+	// listeners
+	OnTouchListener firstValueTouchListener = new OnTouchListener() {
+		
+		@SuppressLint("ClickableViewAccessibility")
+		@Override
+		public boolean onTouch(View v, MotionEvent event) {
+			// TODO Auto-generated method stub
+			vFirstValue.setText("");
+			return false;
+		}
+	};
+	
+	OnTouchListener secondValueTouchListener = new OnTouchListener() {
+		
+		@SuppressLint("ClickableViewAccessibility")
+		@Override
+		public boolean onTouch(View v, MotionEvent event) {
+			// TODO Auto-generated method stub
+			vFirstValue.setText("");
+			return false;
+		}
+	};
+	
+	/* XML components declaration */
+	EditText vFirstValue;
+	EditText vSecondValue;
+	TextView vResult;
+	Button vCalculate;
+	
+	private void initXMLComponents(){
+		vFirstValue = (EditText) findViewById(R.id.editTxt01);
+		vFirstValue.setText("0");
+		vFirstValue.setOnTouchListener(firstValueTouchListener);
+		
+		vSecondValue = (EditText) findViewById(R.id.editTxt02);
+		vSecondValue.setText("0");
+		vSecondValue.setOnTouchListener(secondValueTouchListener);
 	}
 }
